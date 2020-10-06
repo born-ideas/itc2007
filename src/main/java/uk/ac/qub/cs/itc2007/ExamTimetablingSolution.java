fd27df4dca827e003efe935034bbb725f7834404
marcus-bornman <marcus.bornman@frei.one>
2020-10-06 07:23:41 +0200

    Added private fields for hard and soft constraint calculations to ensure they only happen once

--- src/main/java/uk/ac/qub/cs/itc2007/ExamTimetablingSolution.java
+++ src/main/java/uk/ac/qub/cs/itc2007/ExamTimetablingSolution.java
@@ -28,67 +28,76 @@ public class ExamTimetablingSolution {
 
 	/**
 	 * The number of conflicting exams in this solution.
-	 *
+	 * <p>
 	 * See 'Conflicts' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
-	public int conflictingExams() {
-		int count = 0;
+	private int conflictingExams = -1;
 
+	public int conflictingExams() {
+		if (conflictingExams != -1) return conflictingExams;
+
+		conflictingExams = 0;
 		for (Booking bookingA : bookings) {
 			for (Booking bookingB : bookings) {
 				if (bookingA.equals(bookingB)) continue;
 				boolean doClash = bookingA.period.number == bookingB.period.number;
 				boolean doShareStudents = problem.clashMatrix[bookingA.exam.number][bookingB.exam.number] > 0;
-				if (doClash && doShareStudents) count++;
+				if (doClash && doShareStudents) conflictingExams++;
 			}
 		}
-
-		return count;
+		return conflictingExams;
 	}
 
 	/**
 	 * The number of overbooked periods in this solution.
-	 *
+	 * <p>
 	 * See 'RoomOccupancy' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
-	public int overbookedPeriods() {
-		int count = 0;
+	private int overbookedPeriods = -1;
 
+	public int overbookedPeriods() {
+		if (overbookedPeriods != -1) return overbookedPeriods;
+
+		overbookedPeriods = 0;
 		for (Period period : problem.periods) {
 			for (Room room : problem.rooms) {
 				List<Booking> periodRoomBookings = bookings.stream().filter(b -> b.period.number == period.number && b.room.number == room.number).collect(Collectors.toList());
 				int numSeatsNeeded = periodRoomBookings.stream().mapToInt(b -> b.exam.students.size()).sum();
-				if (numSeatsNeeded > room.capacity) count++;
+				if (numSeatsNeeded > room.capacity) overbookedPeriods++;
 			}
 		}
-
-		return count;
+		return overbookedPeriods;
 	}
 
 	/**
 	 * The number of periods that are too short in this solution.
-	 *
+	 * <p>
 	 * See 'PeriodUtilisation' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
-	public int tooShortPeriods() {
-		int count  = 0;
+	private int tooShortPeriods = -1;
 
+	public int tooShortPeriods() {
+		if (tooShortPeriods != -1) return tooShortPeriods;
+
+		tooShortPeriods = 0;
 		HashSet<Booking> seen = new HashSet<>();
 		for (Booking booking : bookings) {
-			if (booking.exam.duration > booking.period.duration && seen.add(booking)) count++;
+			if (booking.exam.duration > booking.period.duration && seen.add(booking)) tooShortPeriods++;
 		}
-
-		return count;
+		return tooShortPeriods;
 	}
 
 	/**
 	 * The number of period hard constraint violations in this solution.
-	 *
+	 * <p>
 	 * See 'PeriodRelated' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
-	public int periodConstraintViolations() {
-		int count = 0;
+	private int periodConstraintViolations = -1;
 
+	public int periodConstraintViolations() {
+		if (periodConstraintViolations != -1) return periodConstraintViolations;
+
+		periodConstraintViolations = 0;
 		for (PeriodHardConstraint constraint : problem.periodHardConstraints) {
 			Booking bookingOne = bookings.stream().filter(b -> b.exam.number == constraint.examOneNum).findFirst().orElse(null);
 			Booking bookingTwo = bookings.stream().filter(b -> b.exam.number == constraint.examTwoNum).findFirst().orElse(null);
@@ -96,119 +105,134 @@ public class ExamTimetablingSolution {
 
 			if (constraint.constraintType.equals("EXAM_COINCIDENCE")) {
 				if (problem.clashMatrix[constraint.examOneNum][constraint.examTwoNum] > 0) continue;
-				if (bookingOne.period.number != bookingTwo.period.number) count++;
+				if (bookingOne.period.number != bookingTwo.period.number) periodConstraintViolations++;
 			}
 
 			if (constraint.constraintType.equals("EXCLUSION")) {
-				if (bookingOne.period.number == bookingTwo.period.number) count++;
+				if (bookingOne.period.number == bookingTwo.period.number) periodConstraintViolations++;
 			}
 
 			if (constraint.constraintType.equals("AFTER")) {
-				if (bookingOne.period.getDateTime().isAfter(bookingTwo.period.getDateTime())) count++;
+				if (bookingOne.period.getDateTime().isAfter(bookingTwo.period.getDateTime()))
+					periodConstraintViolations++;
 			}
 		}
-
-		return count;
+		return periodConstraintViolations;
 	}
 
 	/**
 	 * The number of room hard constraint violations in this solution.
-	 *
+	 * <p>
 	 * See 'RoomRelated' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
-	public int roomConstraintViolations() {
-		int count = 0;
+	private int roomConstraintViolations = -1;
 
+	public int roomConstraintViolations() {
+		if (roomConstraintViolations != -1) return roomConstraintViolations;
+
+		roomConstraintViolations = 0;
 		for (RoomHardConstraint constraint : problem.roomHardConstraints) {
 			if (constraint.constraintType.equals("ROOM_EXCLUSIVE")) {
 				Booking booking = bookings.stream().filter(b -> b.exam.number == constraint.examNum).findFirst().orElse(null);
 				if (booking == null) continue;
 				boolean isNotBookedAlone = bookings.stream().anyMatch(b -> b.room.number == booking.room.number && b.period.number == booking.period.number);
-				if (isNotBookedAlone) count++;
+				if (isNotBookedAlone) roomConstraintViolations++;
 			}
 		}
-
-		return count;
+		return roomConstraintViolations;
 	}
 
 	/**
 	 * The number of occurrences where two examinations are taken by students straight after one another.
-	 *
+	 * <p>
 	 * See 'Two Exams in a Row' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int twoInARowPenalty = -1;
+
 	public int twoInARowPenalty() {
+		if (twoInARowPenalty != -1) return twoInARowPenalty;
+
 		InstitutionalWeighting weighting = problem.institutionalWeightings.stream().filter(w -> w.weightingType.equals("TWOINAROW")).findFirst().orElse(null);
 		if (weighting == null) return 0;
 
-		int penalty = 0;
+		twoInARowPenalty = 0;
 		for (Booking bookingA : bookings) {
 			for (Booking bookingB : bookings) {
 				boolean areInARow = Math.abs(bookingA.period.number - bookingB.period.number) == 1;
 				boolean areOnSameDay = bookingA.period.date.isEqual(bookingB.period.date);
 				if (areInARow && areOnSameDay)
-					penalty += weighting.paramOne * problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
+					twoInARowPenalty += weighting.paramOne * problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
 			}
 		}
-
-		return penalty;
+		return twoInARowPenalty;
 	}
 
 	/**
 	 * The number of occurrences of students having two exams in a day which are not directly adjacent.
-	 *
+	 * <p>
 	 * See 'Two Exams in a Day' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int twoInADayPenalty = -1;
+
 	public int twoInADayPenalty() {
+		if (twoInADayPenalty != -1) return twoInADayPenalty;
+
 		InstitutionalWeighting weighting = problem.institutionalWeightings.stream().filter(w -> w.weightingType.equals("TWOINADAY")).findFirst().orElse(null);
 		if (weighting == null) return 0;
 
-		int penalty = 0;
+		twoInADayPenalty = 0;
 		for (Booking bookingA : bookings) {
 			for (Booking bookingB : bookings) {
 				if (bookingA.equals(bookingB)) continue;
 				boolean areNotAdjacent = Math.abs(bookingA.period.number - bookingB.period.number) != 1;
 				boolean areOnSameDay = bookingA.period.date.isEqual(bookingB.period.date);
 				if (areNotAdjacent && areOnSameDay)
-					penalty += weighting.paramOne * problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
+					twoInADayPenalty += weighting.paramOne * problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
 			}
 		}
-
-		return penalty;
+		return twoInADayPenalty;
 	}
 
 	/**
 	 * Occurrences of enrolled students who have to sit other exams within the desired period spread.
-	 *
+	 * <p>
 	 * See 'Period Spread' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int periodSpreadPenalty = -1;
+
 	public int periodSpreadPenalty() {
+		if (periodSpreadPenalty != -1) return periodSpreadPenalty;
+
 		InstitutionalWeighting weighting = problem.institutionalWeightings.stream().filter(w -> w.weightingType.equals("PERIOD_SPREAD")).findFirst().orElse(null);
 		if (weighting == null) return 0;
 
-		int penalty = 0;
+		periodSpreadPenalty = 0;
 		for (Booking bookingA : bookings) {
 			for (Booking bookingB : bookings) {
 				int spread = bookingB.period.number - bookingA.period.number;
 				if (spread <= 0) continue;
 				boolean areWithinSpread = spread <= weighting.paramOne;
 				if (areWithinSpread)
-					penalty += problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
+					periodSpreadPenalty += problem.clashMatrix[bookingA.exam.number][bookingB.exam.number];
 			}
 		}
-
-		return penalty;
+		return periodSpreadPenalty;
 	}
 
 	/**
 	 * This applies a penalty to a ROOM and PERIOD (not Exam) where there are mixed durations
-	 *
+	 * <p>
 	 * See 'Mixed Durations' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int mixedDurationsPenalty = -1;
+
 	public int mixedDurationsPenalty() {
+		if (mixedDurationsPenalty != -1) return mixedDurationsPenalty;
+
 		InstitutionalWeighting weighting = problem.institutionalWeightings.stream().filter(w -> w.weightingType.equals("NONMIXEDDURATIONS")).findFirst().orElse(null);
 		if (weighting == null) return 0;
 
-		int penalty = 0;
+		mixedDurationsPenalty = 0;
 		for (Period period : problem.periods) {
 			for (Room room : problem.rooms) {
 				List<Booking> myBookings = bookings.stream().filter(b -> b.period.number == period.number && b.room.number == room.number).collect(Collectors.toList());
@@ -216,19 +240,22 @@ public class ExamTimetablingSolution {
 				HashSet<Object> seen = new HashSet<>();
 				myBookings.removeIf(b -> !seen.add(b.exam.duration));
 				int numDifferentDurations = myBookings.size();
-				penalty += (numDifferentDurations - 1) * weighting.paramOne;
+				mixedDurationsPenalty += (numDifferentDurations - 1) * weighting.paramOne;
 			}
 		}
-
-		return penalty;
+		return mixedDurationsPenalty;
 	}
 
 	/**
 	 * This applies a penalty for largest numbers of students are timetabled at the beginning of the examination session.
-	 *
+	 * <p>
 	 * See 'Larger Exams Constraints' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int frontloadPenalty = -1;
+
 	public int frontloadPenalty() {
+		if (frontloadPenalty != -1) return frontloadPenalty;
+
 		InstitutionalWeighting weighting = problem.institutionalWeightings.stream().filter(w -> w.weightingType.equals("FRONTLOAD")).findFirst().orElse(null);
 		if (weighting == null) return 0;
 
@@ -239,46 +266,50 @@ public class ExamTimetablingSolution {
 		if (lastPeriodIndex < 0) lastPeriodIndex = 0;
 		List<Period> lastPeriods = problem.periods.subList(lastPeriodIndex, problem.periods.size());
 
-		int penalty = 0;
+		frontloadPenalty = 0;
 		for (Exam exam : largestExams) {
 			Booking examBooking = bookings.stream().filter(b -> b.exam.number == exam.number).findFirst().orElse(null);
 			if (examBooking == null) continue;
 			boolean isInLastPeriods = lastPeriods.stream().anyMatch(p -> p.number == examBooking.period.number);
-			if (isInLastPeriods) penalty += weighting.paramThree;
+			if (isInLastPeriods) frontloadPenalty += weighting.paramThree;
 		}
 
-		return penalty;
+		return frontloadPenalty;
 	}
 
 	/**
 	 * For each period, if a room used within the solution has an associated penalty, the penalty for that room for that
 	 * period is calculated by multiplying the associated penalty by the number of times the room is used.
-	 *
+	 * <p>
 	 * See 'Room Penalty' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int roomPenalty = -1;
+
 	public int roomPenalty() {
-		int penalty = 0;
+		if (roomPenalty != -1) return roomPenalty;
 
+		roomPenalty = 0;
 		for (Booking booking : bookings) {
-			penalty += booking.room.penalty;
+			roomPenalty += booking.room.penalty;
 		}
-
-		return penalty;
+		return roomPenalty;
 	}
 
 	/**
 	 * For each period the penalty is calculated by multiplying the associated penalty by the number of times the exams
 	 * timetabled within that period.
-	 *
+	 * <p>
 	 * See 'Period Penalty' at http://www.cs.qub.ac.uk/itc2007/examtrack/exam_track_index_files/examevaluation.htm.
 	 */
+	private int periodPenalty = -1;
+
 	public int periodPenalty() {
-		int penalty = 0;
+		if (periodPenalty != -1) return periodPenalty;
 
+		periodPenalty = 0;
 		for (Booking booking : bookings) {
-			penalty += booking.period.penalty;
+			periodPenalty += booking.period.penalty;
 		}
-
-		return penalty;
+		return periodPenalty;
 	}
 }
